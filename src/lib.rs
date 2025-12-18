512bf12f4f3bb608ebc77fde0ea7933282fd2eac
François Gibier <francoisgibier.contact@gmail.com>
2025-12-18 22:30:30 +0100

    put consumer macro in it's own project

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod message;
 mod topic;
 
-pub use broker_macro::{broker, consumer};
+pub use broker_macro::broker;
+pub use consumer_macro::consumer;
 pub use message::Message;
 pub use topic::Topic;
