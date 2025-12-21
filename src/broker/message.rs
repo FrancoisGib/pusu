547d5991598ad18102add029863b9b331bcf06fa
François Gibier <francoisgibier.contact@gmail.com>
2025-12-21 02:57:57 +0100

    comment Response

--- src/broker/message.rs
+++ src/broker/message.rs
@@ -1,12 +1,10 @@
-use serde::{Deserialize, Serialize};
-
 pub struct Message<T> {
     pub id: usize,
     pub payload: T,
 }
 
-#[derive(Serialize, Deserialize)]
-pub enum Response {
-    ACK,
-    FAILED,
-}
+// #[derive(Serialize, Deserialize)]
+// pub enum Response {
+//     ACK,
+//     FAILED,
+// }
